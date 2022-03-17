33b58e1ef516552b4712475fb6c727ef17ec989b
Étienne Bouchard <etiennebouchard7@gmail.com>
2022-03-17 05:52:37 -0400

    Translation feature upgrade

--- lib/models/translated_string.dart
+++ lib/models/translated_string.dart
@@ -1,12 +1,45 @@
-
 class TranslatedString {
-  Map<String, String> translations = {};
+  Map<String, String?> _translations = {};
 
-  TranslatedString(this.translations);
+  TranslatedString(this._translations);
 
   factory TranslatedString.fromJson(Map<String, dynamic> json) {
-    return TranslatedString(json.cast<String, String>());
+    return TranslatedString(json.cast<String, String?>());
   }
 
-  Map<String, dynamic> toJson() => translations.cast<String, dynamic>();
+  // String string(String lang) {
+//
+  // }
+
+  String? operator [](String languageCode) {
+    String? translation = _translations[languageCode];
+
+    if (translation == null && languageCode.contains('_')) {
+      translation = _translations[languageCode.split('_')[0]];
+    }
+
+    if (translation == null && _translations['en'] != null) {
+      translation = _translations['en'];
+    }
+
+    if (translation == null && _translations['en_ca'] != null) {
+      translation = _translations['en_ca'];
+    }
+
+    if (translation == null) {
+      _translations.forEach((key, value) {
+        if (value != null) {
+          translation = value;
+        }
+      });
+    }
+
+    return languageCode;
+  }
+
+  void setTranslation(String languageCode, String? value) {
+    _translations[languageCode] = value;
+  }
+
+  Map<String, dynamic> toJson() => _translations.cast<String, dynamic>();
 }
