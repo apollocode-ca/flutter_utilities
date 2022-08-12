52b17201d95278c1c96cee2e94ea5ebb5bd7ddc7
eduboysem <109082123+eduboysem@users.noreply.github.com>
2022-08-12 16:40:00 -0400

    Adding the lock icon

---
+++ lib/widgets/icons/lock_icon.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LockIcon extends StatefulWidget {
+  const LockIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<LockIcon> createState() => _State();
+}
+
+class _State extends State<LockIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      'icons/lock_icon.svg',
+      clipBehavior: Clip.antiAlias,
+    );
+  }
+}
