cbf319d8ab2c3c6888831dbbc41b349641d55b5c
eduboysem <109082123+eduboysem@users.noreply.github.com>
2022-08-15 17:27:51 -0400

    Adding isDragged to the MaterialStatesHelper

--- lib/helpers/material_states_helper.dart
+++ lib/helpers/material_states_helper.dart
@@ -3,3 +3,7 @@ import 'package:flutter/material.dart';
 bool isHovered(Set<MaterialState> states) {
   return states.contains(MaterialState.hovered);
 }
+
+bool isDragged(Set<MaterialState> states) {
+  return states.contains(MaterialState.dragged);
+}
