d1dc49745812abd8b66b4d90433c0a88ec31fdbb
eduboysem <109082123+eduboysem@users.noreply.github.com>
2022-08-16 13:06:57 -0400

    Transforming the I10nHelper into a singleton

--- lib/helpers/i10n_helper.dart
+++ lib/helpers/i10n_helper.dart
@@ -1,11 +1,36 @@
 import 'package:apollocode_flutter_utilities/errors/i10n_error.dart';
 
-String getNullSafeLabel(String? label, String labelName, String locale) {
-  if (label != null) {
-    return label;
+class I10nHelper {
+  static late final I10nHelper _instance;
+  static var _isInitialized = false;
+
+  static I10nHelper get instance {
+    if (_isInitialized) {
+      return _instance;
+    }
+    throw StateError(
+      'I10nHelper must be initialized before to get an instance.',
+    );
   }
-  throw I10nError(
-    labelName: labelName,
-    locale: locale,
-  );
+
+  static void initialize(String locale) {
+    _instance = I10nHelper._(locale);
+    _isInitialized = true;
+  }
+
+  late final String _locale;
+
+  I10nHelper._(String locale) {
+    _locale = locale;
+  }
+
+  String getLabel(String? label, String labelName) {
+    if (label != null) {
+      return label;
+    }
+    throw I10nError(
+      labelName: labelName,
+      locale: _locale,
+    );
+  }
 }
