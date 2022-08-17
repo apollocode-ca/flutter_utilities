394fa86da770452ecf07eb59ccc08b4e5657c056
eduboysem <109082123+eduboysem@users.noreply.github.com>
2022-08-17 15:44:28 -0400

    Adding a Vector3Extension to scale Vector3 with a DimensionsHelper

---
+++ lib/extensions/vector3_extension.dart
@@ -0,0 +1,16 @@
+import 'package:apollocode_flutter_utilities/helpers/dimensions_helper.dart';
+import 'package:vector_math/vector_math.dart';
+
+extension Vector3Extension on Vector3 {
+  Vector3 scaleFrom(DimensionsHelper helper) {
+    return Vector3(
+      helper.scaleWidthFrom(
+        figmaWidth: x,
+      ),
+      helper.scaleHeightFrom(
+        figmaHeight: y,
+      ),
+      z,
+    );
+  }
+}
