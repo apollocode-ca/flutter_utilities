11d245ac9eca1970cf48ff883888d3de547b9e9b
eduboysem <109082123+eduboysem@users.noreply.github.com>
2022-08-24 12:55:44 -0400

    Adding an optional size for the lock icon

--- lib/widgets/icons/lock_icon.dart
+++ lib/widgets/icons/lock_icon.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class LockIcon extends StatefulWidget {
+  final Size? size;
+
   const LockIcon({
+    this.size,
     Key? key,
   }) : super(key: key);
 
@@ -11,12 +14,27 @@ class LockIcon extends StatefulWidget {
 }
 
 class _State extends State<LockIcon> {
-  @override
-  Widget build(BuildContext context) {
+  Widget get icon {
     return SvgPicture.asset(
       'assets/icons/lock_icon.svg',
       clipBehavior: Clip.antiAlias,
       package: 'apollocode_flutter_utilities',
     );
+  }
+
+  Size? get size {
+    return widget.size;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = this.size;
+    if (size != null) {
+      return ConstrainedBox(
+        child: icon,
+        constraints: BoxConstraints.loose(size),
+      );
+    }
+    return icon;
   }
 }
