c33933df48c4e92e4b831ef982995764ebba8d82
eduboysem <109082123+eduboysem@users.noreply.github.com>
2022-08-25 11:35:18 -0400

    Reverting the last commit

--- lib/models/responsive_models/responsive_border.dart
+++ lib/models/responsive_models/responsive_border.dart
@@ -2,7 +2,7 @@ import 'package:apollocode_flutter_utilities/models/responsive.dart';
 import 'package:flutter/material.dart';
 
 class ResponsiveBorder extends Responsive<Border> {
-  const ResponsiveBorder({
+  ResponsiveBorder({
     required Border desktop,
     required Border mobile,
   }) : super(desktop, mobile);
