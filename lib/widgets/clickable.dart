ad6a193b8c538c0c2e004ec52bb59260ad43c064
eduboysem <109082123+eduboysem@users.noreply.github.com>
2022-08-25 12:41:07 -0400

    Making the cursor optional for the Clickable widget

--- lib/widgets/clickable.dart
+++ lib/widgets/clickable.dart
@@ -4,34 +4,40 @@ import 'package:flutter/services.dart';
 
 class Clickable extends StatelessWidget {
   final Widget child;
-  final SystemMouseCursor cursor;
-  final VoidCallback onTap;
-
+  final SystemMouseCursor? cursor;
   final void Function(PointerEnterEvent)? onEnter;
   final void Function(PointerExitEvent)? onExit;
   final void Function(PointerHoverEvent)? onHover;
+  final void Function() onTap;
 
   const Clickable({
     required this.child,
-    required this.cursor,
-    required this.onTap,
+    this.cursor,
     this.onEnter,
     this.onExit,
     this.onHover,
+    required this.onTap,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: cursor,
-      onEnter: onEnter,
-      onExit: onExit,
-      onHover: onHover,
-      child: GestureDetector(
-        onTap: onTap,
-        child: child,
-      ),
+    final cursor = this.cursor;
+    if (cursor != null) {
+      return MouseRegion(
+        cursor: cursor,
+        onEnter: onEnter,
+        onExit: onExit,
+        onHover: onHover,
+        child: GestureDetector(
+          onTap: onTap,
+          child: child,
+        ),
+      );
+    }
+    return GestureDetector(
+      onTap: onTap,
+      child: child,
     );
   }
 }
