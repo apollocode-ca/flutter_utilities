de46e5701fe3506ed7d75cd5cb59be3dd36bedb6
Emmanuel Dubois <edubois@apollocode.ca>
2022-08-29 14:32:45 -0400

    Adding the "zero" constant and the "+" operator to the responsive edge insets model

--- lib/extensions/size_extension.dart
+++ lib/extensions/size_extension.dart
@@ -1,17 +1,7 @@
-import 'dart:math';
-
 import 'package:apollocode_flutter_utilities/helpers/dimensions_helper.dart';
 import 'package:flutter/material.dart';
 
 extension SizeExtension on Size {
-  double get maxDimension {
-    return max(height, width);
-  }
-
-  double get minDimension {
-    return min(height, width);
-  }
-
   Size scaleWith(
     DimensionsHelper helper, {
     Size? maxSize,
@@ -21,8 +11,8 @@ extension SizeExtension on Size {
     if (height == width) {
       dimension = helper.scaleRadiusFrom(
         figmaRadius: height,
-        maxRadius: maxSize?.maxDimension,
-        minRadius: minSize?.minDimension,
+        maxRadius: maxSize?.longestSide,
+        minRadius: minSize?.shortestSide,
       );
     }
     final scaledHeight = helper.scaleHeightFrom(
--- lib/models/responsive_models/responsive_edge_insets.dart
+++ lib/models/responsive_models/responsive_edge_insets.dart
@@ -3,10 +3,22 @@ import 'package:apollocode_flutter_utilities/models/responsive.dart';
 import 'package:flutter/material.dart';
 
 class ResponsiveEdgeInsets extends Responsive<EdgeInsets> {
+  static const zero = ResponsiveEdgeInsets(
+    desktop: EdgeInsets.zero,
+    mobile: EdgeInsets.zero,
+  );
+
   const ResponsiveEdgeInsets({
     required EdgeInsets desktop,
     required EdgeInsets mobile,
   }) : super(desktop, mobile);
+
+  ResponsiveEdgeInsets operator +(ResponsiveEdgeInsets other) {
+    return ResponsiveEdgeInsets(
+      desktop: desktop + other.desktop,
+      mobile: mobile + other.mobile,
+    );
+  }
 
   @override
   EdgeInsets scaleWith(
