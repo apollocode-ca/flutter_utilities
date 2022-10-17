d5fde808a109aae4fd1f8e89e0b02236fb317528
eDuBoySem <109082123+eduboysem@users.noreply.github.com>
2022-10-17 11:25:24 -0400

    Adjusting partially the adaptive breakpoints on Material specs

--- lib/helpers/adaptive.dart
+++ lib/helpers/adaptive.dart
@@ -9,19 +9,19 @@ import 'package:flutter/material.dart';
 ///
 /// Used to build adaptive and responsive layouts.
 bool isDisplayDesktop(BuildContext context) {
-  return getBreakpointEntry(context).range.start > 1366;
+  return getBreakpointEntry(context).range.start > 1240;
 }
 
 bool isDisplayLaptop(BuildContext context) {
   final breakpoint = getBreakpointEntry(context).range.start;
-  return breakpoint > 840 && breakpoint <= 1366;
+  return breakpoint > 903 && breakpoint <= 1240;
 }
 
 /// Returns a boolean value whether the window is considered extra-small size.
 ///
 /// Used to build adaptive and responsive layouts.
 bool isDisplayMobile(BuildContext context) {
-  return getBreakpointEntry(context).range.start <= 425;
+  return getBreakpointEntry(context).range.start <= 599;
 }
 
 /// Returns a boolean value whether the window is considered small size.
@@ -29,7 +29,7 @@ bool isDisplayMobile(BuildContext context) {
 /// Used to build adaptive and responsive layouts.
 bool isDisplayTablet(BuildContext context) {
   final breakpoint = getBreakpointEntry(context).range.start;
-  return breakpoint > 425 && breakpoint <= 840;
+  return breakpoint > 599 && breakpoint <= 903;
 }
 
 /// Returns boolean value whether the window is considered medium size.
