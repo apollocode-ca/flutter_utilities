612c03d020ecbb44ea939f191e5e9edcb2d4061b
eDuBoySem <109082123+eduboysem@users.noreply.github.com>
2022-10-18 17:01:18 -0400

    Adding the Metamask service and the Banner manager

---
+++ lib/managers/banner_manager.dart
@@ -0,0 +1,71 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+
+class BannerManager {
+  final Color? backgroundColor;
+  final Color? errorColor;
+  final Color? iconColor;
+  final EdgeInsetsGeometry? padding;
+  final TextStyle? textStyle;
+
+  BannerManager({
+    this.backgroundColor,
+    this.errorColor,
+    this.iconColor,
+    this.padding,
+    this.textStyle,
+  });
+
+  late Flushbar _flushbar;
+
+  void showErrorBanner({
+    required BuildContext context,
+    required String message,
+  }) {
+    _flushbar = Flushbar(
+      backgroundColor: backgroundColor ?? Theme.of(context).colorScheme.surface,
+      blockBackgroundInteraction: true,
+      flushbarPosition: FlushbarPosition.TOP,
+      icon: Icon(
+        Icons.error_outline_rounded,
+        color: errorColor ?? Theme.of(context).colorScheme.error,
+        size:
+            textStyle?.height ?? Theme.of(context).textTheme.bodyLarge?.height,
+      ),
+      leftBarIndicatorColor: iconColor ?? Theme.of(context).colorScheme.primary,
+      shouldIconPulse: false,
+      mainButton: IconButton(
+        onPressed: () {
+          _flushbar.dismiss();
+        },
+        icon: Icon(
+          Icons.close,
+          color: iconColor ?? Theme.of(context).colorScheme.primary,
+        ),
+      ),
+      messageText: Container(),
+      padding: _getPadding(context),
+      titleText: Text(
+        message,
+        style: textStyle ?? Theme.of(context).textTheme.bodyLarge,
+      ),
+    );
+    _flushbar.show(context);
+  }
+
+  EdgeInsets _getPadding(BuildContext context) {
+    final padding = this.padding;
+    if (padding != null) {
+      return padding.resolve(
+        Directionality.of(context),
+      );
+    }
+    final themePadding = Theme.of(context).bannerTheme.padding;
+    if (themePadding != null) {
+      return themePadding.resolve(
+        Directionality.of(context),
+      );
+    }
+    return const EdgeInsets.all(16);
+  }
+}
---
+++ lib/services/metamask_service.dart
@@ -0,0 +1,75 @@
+import 'package:apollocode_flutter_utilities/managers/banner_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_web3/flutter_web3.dart';
+
+class MetamaskService {
+  final String networkError;
+  final int networkId;
+  final String noAccountError;
+  final String noWalletError;
+  final String walletRejectedError;
+  final String walletRequestError;
+
+  late final BannerManager _bannerManager;
+
+  MetamaskService({
+    Color? bannerBackgroundColor,
+    Color? bannerErrorColor,
+    Color? bannerIconColor,
+    EdgeInsetsGeometry? bannerPadding,
+    TextStyle? bannerTextStyle,
+    required this.networkError,
+    required this.networkId,
+    required this.noAccountError,
+    required this.noWalletError,
+    required this.walletRejectedError,
+    required this.walletRequestError,
+  }) {
+    _bannerManager = BannerManager(
+      backgroundColor: bannerBackgroundColor,
+      errorColor: bannerErrorColor,
+      iconColor: bannerIconColor,
+      padding: bannerPadding,
+      textStyle: bannerTextStyle,
+    );
+  }
+
+  void connectWallet(BuildContext context, void Function(String) callback) {
+    if (Ethereum.isSupported) {
+      ethereum?.getChainId().then((networkId) {
+        if (networkId != this.networkId) {
+          _bannerManager.showErrorBanner(
+            context: context,
+            message: networkError,
+          );
+        }
+        try {
+          ethereum?.requestAccount().then((addresses) {
+            if (addresses.isEmpty) {
+              _bannerManager.showErrorBanner(
+                context: context,
+                message: noAccountError,
+              );
+            }
+            callback(addresses.first);
+          });
+        } on EthereumException {
+          _bannerManager.showErrorBanner(
+            context: context,
+            message: walletRequestError,
+          );
+        } on EthereumUserRejected {
+          _bannerManager.showErrorBanner(
+            context: context,
+            message: walletRejectedError,
+          );
+        }
+      });
+    } else {
+      _bannerManager.showErrorBanner(
+        context: context,
+        message: noWalletError,
+      );
+    }
+  }
+}
