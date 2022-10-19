a901b585ea6177023962f082c1bc55c6087504a9
eDuBoySem <109082123+eduboysem@users.noreply.github.com>
2022-10-19 17:43:18 -0400

    Adding an address field for the metamask notifier

--- lib/models/notifier_models/metamask_notifier.dart
+++ lib/models/notifier_models/metamask_notifier.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 class MetamaskNotifier extends ChangeNotifier {
+  var _address = '';
   var _isConnected = false;
+
+  String get address {
+    return _address;
+  }
 
   bool get isConnected {
     return _isConnected;
+  }
+
+  set address(String value) {
+    _address = value;
+    notifyListeners();
   }
 
   set isConnected(bool value) {
