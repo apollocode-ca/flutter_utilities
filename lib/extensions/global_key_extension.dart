bd7f9898fa6d8a894910f6eabca304a8c9c124bb
Emmanuel Dubois <edubois@apollocode.ca>
2022-11-24 12:34:48 -0500

    Modifying the absolute position getter to provide rather the widget bounds

--- lib/extensions/global_key_extension.dart
+++ lib/extensions/global_key_extension.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 extension GlobalKeyExtension on GlobalKey {
-  Offset? get absolutePosition {
+  Rect? get widgetBounds {
     final renderObject = currentContext?.findRenderObject();
     final matrix = renderObject?.getTransformTo(null);
     final paintBounds = renderObject?.paintBounds;
     if (matrix != null && paintBounds != null) {
       final rect = MatrixUtils.transformRect(matrix, paintBounds);
-      return rect.topLeft;
+      return rect;
     }
     return null;
   }
